966d5a85ed5c6003a24d075e505a720ae554ee2b
Connor Turland <connorturland@gmail.com>
2021-07-18 20:03:52 -0700

    work with happ client call tutorial

--- zomes/foo/src/lib.rs
+++ zomes/foo/src/lib.rs
@@ -1,6 +1,19 @@
 use hdk::prelude::*;
 
+#[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone)]
+pub struct ZomeInput {
+    pub number: i32,
+}
+
+// data we want back from holochain
+#[derive(Serialize, Deserialize, SerializedBytes, Debug, Clone)]
+pub struct ZomeOutput {
+    pub other_number: i32,
+}
+
 #[hdk_extern]
-pub fn foo(_: ()) -> ExternResult<String> {
-    Ok(String::from("foo"))
+pub fn foo(input: ZomeInput) -> ExternResult<ZomeOutput> {
+    Ok(ZomeOutput {
+      other_number: input.number + 10
+    })
 }
